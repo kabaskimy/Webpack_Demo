f8c30d0ec6ce3a0f960d03b71aa449656dd07738
kabaskimy <kabaskimy@gmail.com>
2017-04-04 21:38:27 +0800

    commit

---
+++ app/demo1.js
@@ -0,0 +1,4 @@
+require('style-loader!css-loader!./style.css');
+import demo2 from "./demo2.js";
+document.write('hello world');
+document.write(demo2);
---
+++ app/demo2.js
@@ -0,0 +1,1 @@
+module.exports = "this is from demo2";
